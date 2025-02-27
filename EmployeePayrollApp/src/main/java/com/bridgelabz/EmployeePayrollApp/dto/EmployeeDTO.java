b26a52ebb8cc29e16c7d1527f9f3375b4fc0f061
anubhav0908 <155910565+anubhav0908@users.noreply.github.com>
2025-02-27 11:26:02 +0530

    [Anubhav]Add. Configured Logging Levels and File Logging in application.properties

--- EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeeDTO.java
@@ -1,11 +1,17 @@
 package com.bridgelabz.EmployeePayrollApp.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class EmployeeDTO {
 
+    @NotBlank(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]*$", message = "Name must start with a capital letter and contain only letters")
     private String name;
+
+    @Min(value = 5000, message = "Salary must be at least 5000")
     private double salary;
-    
 }
